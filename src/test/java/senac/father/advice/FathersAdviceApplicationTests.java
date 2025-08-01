fde2eea51bd96f89ec541944d1c399084773298e
Matheus-D-R-Costa <matheusdarosacosta@protonmail.com>
2025-07-31 21:17:39 -0300

    Setup inicial do projeto com Maven, incluindo os arquivos de configuração, estrutura da aplicação e dependências básicas para o spring boot. Adição do modelo advice e da interface do repositório e um simples arquivo JSON com alguns conselhos.

---
+++ src/test/java/senac/father/advice/FathersAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.father.advice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FathersAdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
